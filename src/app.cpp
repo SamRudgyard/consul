59c04cf49e0845241b78dcad8443cef7ed61426f
Sam Rudgyard <sam.rudgyard@icloud.com>
2025-06-07 10:20:03 +0100

    First attempt at paddle movement

--- src/app.cpp
+++ src/app.cpp
@@ -4,7 +4,10 @@
 #include "SDL3/SDL.h"
 #include "SDL3_image/SDL_image.h"
 
+#include <glm/glm.hpp>
+
 using namespace std;
+using namespace glm;
 
 void App::Init(const char* title, int x, int y, int width, int height, Uint32 flags) {
     Log("(SDL) Initialising video subsystem...");
@@ -18,21 +21,39 @@ void App::Init(const char* title, int x, int y, int width, int height, Uint32 fl
 
     if (!window)   Error("(SDL) Window could not be created. " + string(SDL_GetError()));
     if (!renderer) Error("(SDL) Renderer could not be created. " + string(SDL_GetError()));
+
+    Log("Initialising Objects...");
+    paddlePos = vec2(10, height/2);
+    ballPos = vec2(width/2, height/2);
+    Log("Done.");
 }
 
 void App::Run() {
     state = AppState::Loading;
-
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderClear(renderer);
-    SDL_RenderPresent(renderer);
+    // TODO: Any loading should be done here
+    float paddleWidth = 20.f;
+    float paddleHeight = 200.f;
+    float paddleSpeed = 0.f;
+    SDL_FRect paddle = {paddlePos.x - paddleWidth/2, paddlePos.y - paddleHeight/2, paddleWidth, paddleHeight};
+    float ballRadius = 20.f;
+    SDL_FRect ball = {ballPos.x - ballRadius, ballPos.y - ballRadius, ballRadius, ballRadius};
 
     state = AppState::Running;
 
+    const float oneTargetFPS = 1.f/targetFPS;
+
     while (state != AppState::Quitting) {
+        timer.Tick();
+        deltaTime += timer.timeElapsedSecs;
+        Log("timer.timeElapsedSecs = " + to_string(timer.timeElapsedSecs));
+        Log("deltaTime = " + to_string(deltaTime));
+        if (isless(deltaTime, oneTargetFPS)) continue;
+        deltaTime -= oneTargetFPS;
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
         switch (state) {
-            case AppState::Startup:
-                break;
             case AppState::Loading:
                 // TODO: Load assets
                 break;
@@ -46,14 +67,37 @@ void App::Run() {
                 SDL_Event event;
                 while (SDL_PollEvent(&event)) {
                     if (event.type == SDL_EVENT_QUIT) state = AppState::Quitting;
+                    if (event.type == SDL_EVENT_KEY_DOWN) {
+                        if (event.key.key == SDLK_W) paddleSpeed -= 1*oneTargetFPS;
+                        if (event.key.key == SDLK_S) paddleSpeed += 1*oneTargetFPS;
+                    }
                 }
+
+                if (abs(paddleSpeed) < 0.05f) paddleSpeed = 0.f;
+                paddle.y += paddleSpeed;
+                paddleSpeed *= 0.5f;
+                
+                // Set draw colour
+                SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+                // Draw objects
+                SDL_RenderFillRect(renderer, &paddle);
+                SDL_RenderFillRect(renderer, &ball);
+
+                // Log("(Engine) PaddleSpeed = " + to_string(paddleSpeed));
                 break;
             case AppState::GameOver:
                 // TODO: Game over
                 break;
-            case AppState::Quitting:
+            default:
                 break;
         }
-        
+        SDL_RenderPresent(renderer);
     }
+}
+
+App::~App() {
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 }
--- src/app.hpp
+++ src/app.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "SDL3/SDL.h"
+#include "glm/glm.hpp"
+
+using namespace glm;
 
 enum class AppState {
     Startup,
@@ -12,10 +15,23 @@ enum class AppState {
     Quitting
 };
 
+struct Timer {
+    float timeElapsedSecs = 0.f;
+    uint64_t ticks = 0;
+
+    void Tick() {
+        const uint64_t now = SDL_GetPerformanceCounter();
+        const uint64_t delta = now - ticks;
+        static const uint64_t ticksPerSec = SDL_GetPerformanceFrequency();
+        timeElapsedSecs = delta/static_cast<float>(ticksPerSec);
+        ticks = now;
+    }
+};
+
 class App {
 public:
     App() = default;
-    ~App() = default;
+    ~App();
     void Init(const char* title, int x, int y, int width, int height, Uint32 flags);
     void Run();
 
@@ -23,4 +39,10 @@ public:
 private:
     SDL_Window* window;
     SDL_Renderer* renderer;
+    Timer timer;
+    float deltaTime = 0.;
+    int targetFPS = 30;
+
+    vec2 paddlePos;
+    vec2 ballPos;
 };
