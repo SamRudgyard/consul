4f4c53fb73915979f17b834bcb26d02fd758522b
Sam Rudgyard <sam.rudgyard@icloud.com>
2025-07-04 21:33:25 +0100

    Refactor rendering to use `ForEach`

--- src/ecs/systems/render_system.cpp
+++ src/ecs/systems/render_system.cpp
@@ -1,31 +1,19 @@
 #include "render_system.hpp"
 
 void RenderSystem::Render() {
-    for (const auto& entityContainer : entityManager->GetEntities()) {
-        Entity entity = entityContainer.entity;
-        ComponentMask mask = entityContainer.mask;
+    entityManager->ForEach<Transform2D, Rectangle>([&](Transform2D& transform, Rectangle& rect) {
+        SDL_FRect sdlRect;
+        sdlRect.x = transform.position.x;
+        sdlRect.y = transform.position.y;
+        sdlRect.w = rect.width;
+        sdlRect.h = rect.height;
 
-        // Check if entity has both Transform2D and Rectangle components
-        unsigned int transform = componentManager->GetComponentID<Transform2D>();
-        unsigned int rect = componentManager->GetComponentID<Rectangle>();
-
-        if (mask.test(transform) && mask.test(rect)) {
-            Transform2D& transform = componentManager->GetComponent<Transform2D>(entity);
-            Rectangle& rect = componentManager->GetComponent<Rectangle>(entity);
-
-            SDL_FRect sdlRect;
-            sdlRect.x = transform.position.x;
-            sdlRect.y = transform.position.y;
-            sdlRect.w = rect.width;
-            sdlRect.h = rect.height;
-
-            if (rect.isCentred) {
-                sdlRect.x -= rect.width/2;
-                sdlRect.y -= rect.height/2;
-            }
-            
-            SDL_SetRenderDrawColor(renderer, rect.colour.r, rect.colour.g, rect.colour.b, rect.colour.a);
-            SDL_RenderFillRect(renderer, &sdlRect);
+        if (rect.isCentred) {
+            sdlRect.x -= rect.width/2;
+            sdlRect.y -= rect.height/2;
         }
-    }
+        
+        SDL_SetRenderDrawColor(renderer, rect.colour.r, rect.colour.g, rect.colour.b, rect.colour.a);
+        SDL_RenderFillRect(renderer, &sdlRect);
+    });
 }
