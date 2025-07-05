18e8059a774a03cd4a287c1b1b4dbea598ba52c2
Sam Rudgyard <sam.rudgyard@icloud.com>
2025-07-05 14:56:03 +0100

    Reflect on edge of screen with entity's collider, not position

--- src/ecs/systems/collisions.cpp
+++ src/ecs/systems/collisions.cpp
@@ -5,10 +5,12 @@
 #include "../components/physics_2d.hpp"
 
 void Collisions::Update(float deltaTime) {
-    entityManager->ForEach<CollisionEdgeOfScreen, Transform2D, Physics2D>([&](CollisionEdgeOfScreen& collision, Transform2D& transform, Physics2D& physics) {
+    entityManager->ForEach<CollisionEdgeOfScreen, RectCollider, Transform2D, Physics2D>([&](CollisionEdgeOfScreen& collision, RectCollider& collider, Transform2D& transform, Physics2D& physics) {
         if (collision.isEnabled) {
-            if (transform.position.x <= 0.f || transform.position.x >= 800.f) physics.velocity.x *= collision.reflectionMultiplier;
-            if (transform.position.y <= 0.f || transform.position.y >= 600.f) physics.velocity.y *= collision.reflectionMultiplier;
+            vec2 min = transform.position + collider.offset;
+            vec2 max = min + collider.size;
+            if (min.x <= 0.f || max.x >= 800.f) physics.velocity.x *= collision.reflectionMultiplier;
+            if (min.y <= 0.f || max.y >= 600.f) physics.velocity.y *= collision.reflectionMultiplier;
         }
     });
 
