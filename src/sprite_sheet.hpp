8ed24c80aaf10453fc44815dc2d96ab28a209710
Sam Rudgyard <sam.rudgyard@icloud.com>
2025-09-01 19:31:24 +0100

    Implement SpriteSheet and Sprite

---
+++ src/sprite_sheet.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <vector>
+
+#include "texture.hpp"
+#include "sprite.hpp"
+
+class SpriteSheet
+{
+public:
+    SpriteSheet(const char* filePath, int spriteWidth, int spriteHeight, bool requiresAlphaChannel = true);
+    ~SpriteSheet();
+
+    Sprite GetSprite(int index) const;
+private:
+    std::shared_ptr<Texture> texture;
+    std::vector<Sprite> sprites;
+};
