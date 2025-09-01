a178b65c9f702be1d67026a63074a802b094e60a
Sam Rudgyard <sam.rudgyard@icloud.com>
2025-09-01 18:23:55 +0100

    Add shaders and textures + managers

---
+++ src/texture_manager.cpp
@@ -0,0 +1,75 @@
+#include "texture_manager.hpp"
+
+#ifndef STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_IMPLEMENTATION
+#endif
+#include "stb_image.h"
+
+#include "glad/glad.h"
+
+#include "utils.hpp"
+
+Texture TextureManager::LoadTexture(const char* filePath, const char* referenceName, bool requiresAlphaChannel)
+{
+    if (!filePath) Error("[TextureManager::LoadTexture] Texture file path is null");
+
+    // Check if name is already present
+    if (textures.find(referenceName) != textures.end()) LogOnDebug("[TextureManager::LoadTexture] Texture with name '" + std::string(referenceName) + "' is already loaded, will overwrite.");
+
+    if (!DoesFileExist(filePath)) Error("[TextureManager::LoadTexture] Texture file does not exist: '" + std::string(filePath) + "'");
+
+    Texture texture;
+
+    if (requiresAlphaChannel) {
+        texture.internalFormat = GL_RGBA;
+        texture.imageFormat = GL_RGBA;
+    } else {
+        texture.internalFormat = GL_RGB;
+        texture.imageFormat = GL_RGB;
+    }
+
+    int width, height, nrChannels;
+    unsigned char* data = stbi_load(filePath, &width, &height, &nrChannels, 0);
+    texture.Generate(width, height, data);
+    stbi_image_free(data);
+
+    textures[referenceName] = texture;
+
+    LogOnDebug("[TextureManager::LoadTexture] Loaded texture from file '" + std::string(filePath) + "' (ID: " + std::to_string(texture.id) + ", Size: " + std::to_string(width) + "x" + std::to_string(height) + ")");
+
+    return textures[referenceName];
+}
+
+Texture TextureManager::GetTexture(const char* referenceName)
+{
+    if (textures.find(referenceName) == textures.end()) {
+        Error("[TextureManager::GetTexture] Texture with name '" + std::string(referenceName) + "' is not loaded");
+        return Texture();
+    }
+
+    return textures[referenceName];
+}
+
+void TextureManager::UnloadTexture(Texture texture)
+{
+    glDeleteTextures(1, &texture.id);
+
+    // Remove from map
+    for (auto it = textures.begin(); it != textures.end(); ++it) {
+        if (it->second.id == texture.id) {
+            textures.erase(it);
+            break;
+        }
+    }
+
+    LogOnDebug("[TextureManager::UnloadTexture] Unloaded texture with ID " + std::to_string(texture.id));
+}
+
+void TextureManager::Clear()
+{
+    for (auto& pair : textures) {
+        glDeleteTextures(1, &pair.second.id);
+    }
+    textures.clear();
+    LogOnDebug("[TextureManager::Clear] Cleared all textures from memory");
+}
