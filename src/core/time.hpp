ebfe690b6447ef8b1bc66e6028e62c92438883f8
Sam Rudgyard <sam.rudgyard@icloud.com>
2025-12-17 11:17:50 +0000

    Set target FPS to 1/60

--- src/core/time.hpp
+++ src/core/time.hpp
@@ -5,6 +5,6 @@ struct Time
     float currentTime = 0.0f;               // Current time in seconds
     float previousTime = 0.0f;              // Time at the previous frame in seconds
     float deltaTime = 0.0f;                 // Time elapsed since last frame in seconds
-    float targetFrameTime = 1.0f / 2.0f;    // Target frame time for 60 FPS
+    float targetFrameTime = 1.0f / 60.0f;   // Target frame time for 60 FPS
     unsigned int frameCount = 0;            // Total number of frames rendered
 };
