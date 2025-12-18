d0d95209a05c8607dfa7e46e1fcbfefdccc68dd4
Sam Rudgyard <sam.rudgyard@icloud.com>
2025-12-18 16:27:40 +0000

    Separate out unit conversions from constants

--- src/maths/constants.hpp
+++ src/maths/constants.hpp
@@ -1,12 +1,11 @@
 #pragma once
 
-// constexpr float HUGE = 3.402823466e+38f;
-constexpr float TINY = 1.175494351e-38f;
+#include <limits>
+
+constexpr float MASSIVE = std::numeric_limits<float>::max();
+constexpr float TINY = std::numeric_limits<float>::min();
 
 constexpr float PI = 3.14159265358979323846f;
 constexpr float TWO_PI = 6.28318530717958647692f;
 constexpr float EULERS_NUMBER = 2.71828182845904523536f;
 constexpr float GOLDEN_RATIO = 1.618033988749895f;
-
-constexpr float DEG_TO_RAD = PI / 180.0f;
-constexpr float RAD_TO_DEG = 180.0f / PI;
