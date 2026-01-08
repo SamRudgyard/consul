f064c9d04412dff4bb5f69e37842bbe1b0efb91c
Sam Rudgyard <sam.rudgyard@icloud.com>
2026-01-08 20:26:02 +0000

    Add bunch of shapes to display

--- src/main.cpp
+++ src/main.cpp
@@ -25,12 +25,24 @@ int main(int argc, char **argv) {
 
     Geometry* geometry = Geometry::get();
     Mesh cube = geometry->cube(0.5f);
-    cube.setTint(Colour(80, 120, 220));
+    cube.setTint(Colour(126, 0, 126));
     renderer.addMesh(cube, glm::translate(glm::mat4(1.0f), glm::vec3(-1.0f, 0.0f, 0.0f)));
 
-    Mesh sphere = geometry->sphereUV(0.5f, 10, 10);
+    Mesh cylinder = geometry->cylinder(0.5f, 0.2f, 2.0f, 20);
+    cylinder.setTint(Colour(235, 110, 52));
+    renderer.addMesh(cylinder, glm::translate(glm::mat4(1.0f), glm::vec3(0.f, 1.0f, 0.0f)));
+
+    Mesh cone = geometry->cone(1.0f, 2.0f, 20);
+    cone.setTint(Colour(66, 245, 132));
+    renderer.addMesh(cone, glm::translate(glm::mat4(1.0f), glm::vec3(0.f, -2.0f, 0.0f)));
+
+    Mesh sphere = geometry->sphereIcosphere(0.5f, 1);
     sphere.setTint(Colour(80, 120, 220));
     renderer.addMesh(sphere, glm::translate(glm::mat4(1.0f), glm::vec3(1.0f, 0.0f, 0.0f)));
+
+    Mesh pyramid = geometry->pyramidSquare(1.0f, 1.0f);
+    pyramid.setTint(Colour(191, 66, 245));
+    renderer.addMesh(pyramid, glm::translate(glm::mat4(1.0f), glm::vec3(5.0f, 0.0f, 0.0f)));
 
     while (consul.run()) {
         EngineContext* context = EngineContext::get();
