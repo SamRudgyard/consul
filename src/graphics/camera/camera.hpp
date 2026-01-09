a27d021c1d0c4d553ac314f2768196c2611fac85
Sam Rudgyard <sam.rudgyard@icloud.com>
2026-01-09 20:03:22 +0000

    Add `ProjectionType` to `Camera`

--- src/graphics/camera/camera.hpp
+++ src/graphics/camera/camera.hpp
@@ -14,16 +14,28 @@
 
 class IShader;
 
+enum class ProjectionType
+{
+    Perspective,
+    Orthographic
+};
+
 class Camera {
 public:
     float speed = 0.1f;
     float sensitivity = 0.1f;
 
     /**
-     * A perspective camera with given viewport width/height and world position.
+     * A camera with given viewport width/height and world position.
      * @param position World space position of the camera.
      */
     Camera(glm::vec3 position);
+
+    /**
+     * Set the projection type (see `ProjectionType`).
+     * @param projectionType New projection type.
+     */
+    void setProjectionType(ProjectionType projectionType) { this->projectionType = projectionType; }
 
     /**
      * Set the field of view in degrees.
@@ -33,15 +45,21 @@ public:
 
     /**
      * Set the near clipping plane distance.
-     * @param nearPlane Near clipping plane distance.
+     * @param near Near clipping plane distance.
      */
-    void setNearPlane(float nearPlane);
+    void setNearPlane(float near);
 
     /**
      * Set the far clipping plane distance.
-     * @param farPlane Far clipping plane distance.
+     * @param far Far clipping plane distance.
      */
-    void setFarPlane(float farPlane);
+    void setFarPlane(float far);
+
+    /**
+     * Set an orthographic projection volume.
+     * Coordinates are in world units.
+     */
+    void setOrthographic(float left, float right, float bottom, float top);
     
     /**
      * Handle basic WASD + space/ctrl movement and RMB-look input.
@@ -57,13 +75,20 @@ public:
 private:
     EngineContext* context = EngineContext::get();
     glm::vec3 position;
-    float FOVdeg = 45.0f;
-    float nearPlane = 0.1f;
-    float farPlane = 100.0f;
-    glm::vec3 orientation = glm::vec3(0.0f, 0.0f, -1.0f);
-    glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+    ProjectionType projectionType = ProjectionType::Perspective;
+    float near = 0.1f;
+    float far = 100.0f;
+    float left = -1.0f;
+    float right = 1.0f;
+    float bottom = -1.0f;
+    float top = 1.0f;
     glm::mat4 viewMatrix = glm::mat4(1.0f);
     glm::mat4 projectionMatrix = glm::mat4(1.0f);
+
+    // Perspective projection specific
+    float FOVdeg = 45.0f;
+    glm::vec3 orientation = glm::vec3(0.0f, 0.0f, -1.0f);
+    glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
 
     /**
      * Update the projection matrix based on current FOV, near and far planes.
