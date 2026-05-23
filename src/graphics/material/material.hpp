b65ba066e83c450480e239b83490cc9334d980da
Sam Rudgyard <sam.rudgyard@icloud.com>
2026-05-23 12:30:51 +0100

    Docstring `Material` class

--- src/graphics/material/material.hpp
+++ src/graphics/material/material.hpp
@@ -16,17 +16,44 @@ public:
         this->setTexture(Texture::getDefaultSpecularTexture());
     };
 
+    /**
+     * Gets the shader uniforms associated with this material.
+     * @returns Vector of shader uniforms.
+     */
     std::vector<ShaderUniform>& getUniforms() { return uniforms; }
+
+    /**
+     * Gets the shader uniforms associated with this material.
+     * @returns Vector of shader uniforms.
+     */
     const std::vector<ShaderUniform>& getUniforms() const { return uniforms; }
 
+    /**
+     * Sets a shader uniform on this material.
+     * @param name Name of the uniform.
+     * @param value Value of the uniform.
+     */
     void setUniform(const std::string& name, UniformValue value)
     {
         uniforms.push_back({name, value});
     }
 
+    /**
+     * Gets the textures associated with this material.
+     * @returns Vector of textures.
+     */
     std::vector<Texture>& getTextures() { return textures; }
+
+    /**
+     * Gets the textures associated with this material.
+     * @returns Vector of textures.
+     */
     const std::vector<Texture>& getTextures() const { return textures; }
 
+    /**
+     * Sets a texture for this material.
+     * @param texture Texture to set.
+     */
     void setTexture(Texture texture)
     {
         const TextureType textureType = texture.getType();
