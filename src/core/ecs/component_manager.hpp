64b8a863122db706102a846778010eacf634de46
Sam Rudgyard <sam.rudgyard@icloud.com>
2026-06-08 21:17:05 +0100

    Register new components on the fly

--- src/core/ecs/component_manager.hpp
+++ src/core/ecs/component_manager.hpp
@@ -53,23 +53,23 @@ public:
     ComponentManager() = default;
     ~ComponentManager() = default;
 
-    template <typename T>
-    void RegisterComponent() {
-        const std::type_index typeIdx = typeid(T);
-        if (components.find(typeIdx) == components.end()) {
-            components[typeIdx] = std::make_unique<ComponentArray<T>>();
-        }
-    }
-
+    /**
+     * Gets the unique ID for the component type `T`. If this is the first time requesting an ID for this type, a new ID is generated and stored.
+     * @tparam T The component type for which to get the ID.
+     * @returns The unique ID of the component type `T`.
+     */
     template <class T>
     unsigned int GetComponentID() {
         const std::type_index typeIdx = typeid(T);
-        auto it = typeToID.find(typeIdx);
-        if (it != typeToID.end()) return it->second;
 
-        unsigned int newID = nComponents++;
-        typeToID[typeIdx] = newID;
-        return newID;
+        // If not registered, register now
+        if (typeToID.find(typeIdx) == typeToID.end()) {
+            Console::get().logOnDebug("[ComponentManager::GetComponentID] Registering new component of type '" + std::string(typeid(T).name()) + "'");
+            unsigned int newID = nComponents++;
+            typeToID[typeIdx] = newID;
+            components[typeIdx] = std::make_unique<ComponentArray<T>>();
+        }
+        return typeToID[typeIdx];
     }
 
     template<class T>
