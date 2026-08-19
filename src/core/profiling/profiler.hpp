dfa2ce4ec73d9283681c952b56970e127bc25825
Sam Rudgyard <sam.rudgyard@icloud.com>
2026-08-19 07:30:18 +0100

    Make `Profiler` static - fixes `CONSUL_PROFILE_METHOD`

--- src/core/profiling/profiler.hpp
+++ src/core/profiling/profiler.hpp
@@ -13,7 +13,12 @@ public:
     static constexpr float MAX_SECONDS_RECORDED = 60.0f;
     static constexpr std::size_t INVALID_METHOD_INDEX = (std::size_t)(-1);
 
-    Profiler() = default;
+    static Profiler& get()
+    {
+        static Profiler instance;
+        return instance;
+    }
+
     Profiler(const Profiler&) = delete;
     Profiler& operator=(const Profiler&) = delete;
 
@@ -29,6 +34,7 @@ public:
     const std::vector<float>* getMethodHistoryMs(const std::string& methodName) const;
 
 private:
+    Profiler() = default;
     std::size_t ensureMethodIndex(const char* methodName);
 
     std::unordered_map<std::string, std::size_t> methodIndices;
